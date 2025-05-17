8fa228cf9dd5140cb38df9e6fa58f9504726738e
gydnjs <khw33352182@gmail.com>
2025-05-17 20:06:14 +0900

    fix: resolve Addressable build failure by updating LoadPath setting
    Addressable의 profile중 LoadPath때문에 로드를 할 수 없던 문제를 해결했ㅅㅂ니다

--- Assets/@Resources/Script/Loader.cs
+++ Assets/@Resources/Script/Loader.cs
@@ -10,31 +10,30 @@ public class Loader : MonoBehaviour
     ResourceManager resourceManager;
     async void Start()
     {
+        Debug.Log("권한 No");
         WebGLBridge.getUserAuthority();
         while (UserManager.Instance.Authority == UserManager.UserAuthority.None)
         {
             await UniTask.Yield();
         }
-
-
+        Debug.Log("권한 On");
         resourceManager = new ResourceManager();
         resourceManager.LoadAllAsync<GameObject>("Prefab", () =>
         {
+            Debug.Log("로딩1 완료");
             resourceManager.LoadAllAsync<Material>("Material", () =>
             {
-                resourceManager.LoadAllAsync<RenderTexture>("RenderTexture", () =>
+                Debug.Log("로딩2 완료");
+                UserManager.Instance.Init();
+                switch (UserManager.Instance.Authority)
                 {
-                    UserManager.Instance.Init();
-                    switch (UserManager.Instance.Authority)
-                    {
-                        case UserManager.UserAuthority.Create:
-                            SceneManager.LoadScene((int)Scenes.Create);
-                            break;
-                        case UserManager.UserAuthority.Specte:
-                            SceneManager.LoadScene((int)Scenes.Specte);
-                            break;
-                    }
-                });
+                    case UserManager.UserAuthority.Create:
+                        SceneManager.LoadScene((int)Scenes.Create);
+                        break;
+                    case UserManager.UserAuthority.Specte:
+                        SceneManager.LoadScene((int)Scenes.Specte);
+                        break;
+                }
             });
         });
     }
